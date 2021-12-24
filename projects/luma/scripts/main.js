5dcc81a851fb69e2b452da54ae9753baa3cf448f
Denis <93516910+Bussun@users.noreply.github.com>
2021-12-25 00:15:47 +0100

    Website update
    Finishing the page by working on the changelog section
    JSON loading done
    
    Only UI and other minor fixes left

--- projects/luma/scripts/main.js
+++ projects/luma/scripts/main.js
@@ -1,16 +1,25 @@
-const cppChangelogBtn = document.querySelector('#cpp-changelogBtn');
-const cppChangelogTab = document.querySelector('#cpp-changelog');
-const csharpChangelogBtn = document.querySelector('#csharp-changelogBtn');
-const csharpChangelogTab = document.querySelector('#csharp-changelog');
+//c++ tab
+const CppChangelogBtn = document.querySelector('#cpp-changelogBtn');
+const CppChangelogTab = document.querySelector('#cpp-changelog-tab');
+const CppChangelogContainer = document.querySelector('.cpp-changelog');
 
-csharpChangelogBtn.addEventListener('click', () => {
-    cppChangelogTab.style.display = "none";
-    csharpChangelogTab.style.display = "initial";
+//C# tab
+const CSharpChangelogBtn = document.querySelector('#csharp-changelogBtn');
+const CSharpChangelogTab = document.querySelector('#csharp-changelog-tab');
+const CSharpChangelogContainer = document.querySelector('.csharp-changelog');
+
+CSharpChangelogBtn.addEventListener('click', () => {
+    CppChangelogTab.style.display = "none";
+    CSharpChangelogBtn.classList.add("selected");
+    CppChangelogBtn.classList.remove("selected");
+    CSharpChangelogTab.style.display = "flex";
 });
 
-cppChangelogBtn.addEventListener('click', () => {
-    csharpChangelogTab.style.display = "none";
-    cppChangelogTab.style.display = "initial";
+CppChangelogBtn.addEventListener('click', () => {
+    CSharpChangelogTab.style.display = "none";
+    CSharpChangelogBtn.classList.remove("selected");
+    CppChangelogBtn.classList.add("selected");
+    CppChangelogTab.style.display = "flex";
 });
 
 /* Changelog data */ 
@@ -21,71 +30,81 @@ const changelog = `{
             "version": "0.4",
             "comment": "Initial beta release",
             "beta": true,
-            "added": {
-                "1": "Opening messages from ARC",
-                "2": "Escape sequences support",
-                "3": "Message properties editing"
-            },
-            "deleted": "Nothing",
-            "changed": "Nothing"
+            "added": [
+                "Opening messages from ARC",
+                "Escape sequences support",
+                "Message properties editing"
+            ],
+            "deleted": ["Nothing"],
+            "changed": ["Nothing"]
         },
         {
             "version": "0.4.5",
             "comment": "",
             "beta": true,
-            "added": {
-                "1": "Update checker"
-            },
-            "deleted": "Nothing",
-            "changed": {
-                "1": "[TEMPORARY] Improved FLW1 and FLI1 section parsing",
-                "2": "Refactored some code to reduce program size and improve stability"
-            }
+            "added": [
+                "Update checker"
+            ],
+            "deleted": ["Nothing"],
+            "changed": [
+                "[TEMPORARY] Improved FLW1 and FLI1 section parsing",
+                "Refactored some code to reduce program size and improve stability"
+            ]
         },
         {
             "version": "0.6",
             "comment": "",
             "beta": true,
-            "added": {
-                "1": "New message entries supported"
-            },
-            "deleted": "Nothing",
-            "changed": {
-                "1": "Code refactoring"
-            }
+            "added": [
+                "New message entries supported"
+            ],
+            "deleted": ["Nothing"],
+            "changed": [
+                "Code refactoring"
+            ]
         },
         {
             "version": "0.7",
             "comment": "",
             "beta": true,
-            "added": "Nothing",
-            "deleted": "Nothing",
-            "changed": {
-                "1": "UI updates"
-            }
+            "added": ["Nothing"],
+            "deleted": ["Nothing"],
+            "changed": [
+                "UI updates"
+            ]
         },
         {
             "version": "0.7.1",
             "comment": "",
             "beta": true,
-            "added": {
-                "1": "ARC file backup in case of corruption"
-            },
-            "deleted": "Nothing",
-            "changed": "Nothing"
+            "added": [
+                "ARC file backup in case of corruption"
+            ],
+            "deleted": ["Nothing"],
+            "changed": ["Nothing"]
         },
         {
             "version": "1.0",
             "comment": "First release",
             "beta": false,
-            "added": {
-                "1": "Started working on flows"
-            },
-            "deleted": "Nothing",
-            "changed": {
-                "1": "Code refactor",
-                "2": "New updater"
-            }
+            "added": [
+                "Started working on flows"
+            ],
+            "deleted": ["Nothing"],
+            "changed": [
+                "Code refactor",
+                "New updater"
+            ]
+        },
+        {
+            "version": "1.1",
+            "comment": "Unreleased beta",
+            "beta": true,
+            "added": [
+                "[WIP] Little endian support"
+            ],
+            "deleted": ["Nothing"],
+            "changed": ["To be updated soon"]
         }
         
     ],
@@ -104,8 +123,39 @@ csChglog = parsedThing.CSharp;
 csChglog.forEach(obj => {
     const versionDiv = document.createElement('div');
     versionDiv.classList.add("version");
+    //Version
     const versionTag = document.createElement('p');
     versionTag.innerText = obj.version;
     versionDiv.appendChild(versionTag);
-    csharpChangelogTab.appendChild(versionDiv);  
+
+    //Comment
+    const commentTag = document.createElement('p');
+    commentTag.innerText = obj.comment;
+    versionDiv.appendChild(commentTag);
+
+    //Code executed depending on beta status
+    if (obj.beta) { versionDiv.classList.add("beta"); }
+
+    //Added
+    obj.added.forEach(addedStuff => {
+        const addedStuffP = document.createElement('p');
+        addedStuffP.innerText = addedStuff;
+        versionDiv.appendChild(addedStuffP);
+    });
+
+    //Deleted
+    obj.deleted.forEach(deletedStuff => {
+        const deletedStuffP = document.createElement('p');
+        deletedStuffP.innerText = deletedStuff;
+        versionDiv.appendChild(deletedStuffP);
+    });
+
+    //Changed
+    obj.changed.forEach(changedStuff => {
+        const changedStuffP = document.createElement('p');
+        changedStuffP.innerText = changedStuff;
+        versionDiv.appendChild(changedStuffP);
+    });
+
+    CSharpChangelogContainer.appendChild(versionDiv);  
 });
